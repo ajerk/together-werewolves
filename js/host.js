f1f04c445f95aaa31e25fd62afbebb1b1f03cb82
ajerk <zhuangruijian@qq.com>
2016-10-07 22:10:32 +0800

    ajerk

--- js/host.js
+++ js/host.js
@@ -1,6 +1,8 @@
 //主持人
 
+
 function host(){
+    
     
     //主持人主持哪一个桌子
     this.table = undefined;
@@ -16,6 +18,7 @@ function host(){
     //天黑请闭眼
     this.nightcoming = function(){
         
+        day++;
         //要先重置昨夜的死亡情况
         this.msgrecord.lastnightkilledid = 0;
         this.msgrecord.witchkillid = 0;
@@ -36,24 +39,22 @@ function host(){
     
     //宣布死亡情况
     this.anouncedead = function(){
-
         if(this.msgrecord.witchkillid == 0 && this.msgrecord.lastnightkilledid == 0){
             addMsg("昨夜平安夜");
-            
-        }else if(this.msgrecord.lastnightkilledid != 0 && this.msgrecord.witchkillid == 0){
+        }else if(this.msgrecord.lastnightkilledid !== 0 && this.msgrecord.witchkillid == 0){
             addMsg("昨夜,"+this.msgrecord.lastnightkilledid+"号玩家死亡");
-        }else if(this.msgrecord.lastnightkilledid == 0 && this.msgrecord.witchkillid != 0){
+        }else if(this.msgrecord.lastnightkilledid == 0 && this.msgrecord.witchkillid !== 0){
             addMsg("昨夜,"+this.msgrecord.witchkillid+"号玩家死亡");
-        }else if(this.msgrecord.lastnightkilledid != 0 && this.msgrecord.witchkillid != 0){
+        }else if(this.msgrecord.lastnightkilledid !== 0 && this.msgrecord.witchkillid !== 0){
             addMsg("昨夜双死,"+this.msgrecord.lastnightkilledid+"号 "+this.msgrecord.witchkillid+"号玩家死亡");
         }
-        
     }
     
     
     //统计投票结果
+    var maxid = 0;  //票数最高的id
     this.calvote = function(){
-        var maxid = 0; //票数最高的id
+        maxid = 0;      
         var maxcount = 0; //最高的票数
         for(var i=0;i<this.table.peoplecount;i++){
             var person = this.table.persons[i]; //取得第i个人
@@ -71,12 +72,17 @@ function host(){
         this.table.persons[maxid-1].alive = false; //出局设置为false
     }
     
+    
+    
+    var godcount = 0; //神民数量
+    var villagercount = 0; //平民数量
+    var werewolvescount = 0; //狼人数量
     //核算游戏结果
     this.checkresult = function(){
-        var godcount = 0; //神民数量
-        var villagercount = 0; //平民数量
-        var werewolvescount = 0; //狼人数量
         
+        godcount = 0;
+        villagercount = 0; 
+        werewolvescount = 0; 
         for(var i=0;i<this.table.peoplecount;i++){
             var person = this.table.persons[i]; //取得第i个人
             if(!person.alive){
@@ -101,4 +107,34 @@ function host(){
         
     }
     
+    
+    
+    //天数 
+    this.day = function(day){
+        if(day == 0)
+            $("#messagebox").append("<br><br>游戏开始<br><br>");
+        else 
+            $("#messagebox").append("<br><br>第"+day+"天<br><br>");
+    }
+    
+    
+    //死者发表遗言
+    this.thedeadlastwords = function(){
+        if(day == 1 && maxid == 0 ){
+            if(this.msgrecord.lastnightkilledid !== 0 && this.msgrecord.witchkillid !== 0){
+                addMsg("请");
+                if(this.msgrecord.lastnightkilledid!==0)
+                   addMsg(this.msgrecord.lastnightkilledid);
+                if(this.msgrecord.lastnightkilledid !== 0 && this.msgrecord.witchkillid == 0)
+                    addMsg("号玩家和");
+                if(this.msgrecord.witchkillid!==0)
+                   addMsg(this.msgrecord.witchkillid);
+                addMsg("号玩家发表遗言");
+            }
+        }
+        else if(day == 1 && maxid !== 0 )
+            addMsg("请"+maxid+"号玩家发表遗言");
+        else if( werewolvescount !== 0 && godcount+villagercount !== 0)
+            addMsg("请"+maxid+"号玩家发表遗言");
+    }
 }
